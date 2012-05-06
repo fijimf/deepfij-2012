81d0655ab27c4d98efd92f416cf79a5967db1909
fijimf <fijimf@gmail.com>
2012-05-06 09:29:56 -0400

    Halfway there

--- deepfij-web/src/main/scala/com/fijimf/deepfij/server/AdminRoute.scala
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/server/AdminRoute.scala
@@ -3,33 +3,55 @@ package com.fijimf.deepfij.server
 import cc.spray._
 import cc.spray.http.MediaTypes.`text/html`
 import cc.spray.Route
-import com.fijimf.deepfij.modelx.TeamDao
 import com.fijimf.deepfij.server.Util._
-import com.fijimf.deepfij.view.{AdminPanel, MissingResourcePanel, BasePage, TeamPanel}
+import com.fijimf.deepfij.view.{AdminPanel, BasePage}
 import com.fijimf.deepfij.data.ncaa.NcaaTeamScraper
 import com.fijimf.deepfij.data.kenpom.KenPomScraper
-import com.fijimf.deepfij.workflow.Scraper
+import org.apache.log4j.Logger
+import com.fijimf.deepfij.workflow.{FullRebuild, Scraper}
+import java.text.SimpleDateFormat
 
 object AdminRoute extends Directives {
-  val value=  Scraper(NcaaTeamScraper, NcaaTeamScraper, KenPomScraper("http://kenpom.com/cbbga12.txt", "kenpom.alias.txt"))
+  val log = Logger.getLogger(this.getClass)
+  val scraper = Scraper(NcaaTeamScraper, NcaaTeamScraper, KenPomScraper("http://kenpom.com/cbbga12.txt", "kenpom.alias.txt"))
+  val yyyymmdd = new SimpleDateFormat("yyyyMMdd")
+
   def apply(): Route = {
     pathPrefix("admin") {
       path("new") {
         get {
-          respondWithMediaType(`text/html`) {
-            _.complete(html5Wrapper(BasePage(title = "Deep Fij Admin", content = Some(AdminPanel())))
-            )
-          }
-      }
-      }~ path("rebuild"){
-        get {
-          respondWithMediaType(`text/html`) {
-            _.complete(html5Wrapper(BasePage(title = "Deep Fij Admin", content = Some(AdminPanel())))
-            )
+          detach {
+            parameters('key, 'name, 'from, 'to) {
+              (key: String, name: String, from: String, to: String) => {
+                log.info("Key=" + key)
+                log.info("Name=" + name)
+                log.info("From=" + from)
+                log.info("To=" + to)
+                create(key, name, from, to)
+                respondWithMediaType(`text/html`) {
+                  _.complete(html5Wrapper(BasePage(title = "Deep Fij Admin", content = Some(AdminPanel())))
+                  )
+                }
+              }
+            }
           }
         }
-      } ~
-      get {
+      } ~ path("rebuild") {
+        get {
+          parameters('key, 'name, 'from, 'to) {
+            (key: String, name: String, from: String, to: String) => {
+              log.info("Key=" + key)
+              log.info("Name=" + name)
+              log.info("From=" + from)
+              log.info("To=" + to)
+              respondWithMediaType(`text/html`) {
+                _.complete(html5Wrapper(BasePage(title = "Deep Fij Admin", content = Some(AdminPanel())))
+                )
+              }
+            }
+          }
+        }
+      } ~ get {
         respondWithMediaType(`text/html`) {
           _.complete(html5Wrapper(BasePage(title = "Deep Fij Admin", content = Some(AdminPanel())))
           )
@@ -37,4 +59,25 @@ object AdminRoute extends Directives {
       }
     }
   }
+
+
+  def create(key: String, name: String, from: String, to: String) {
+    val fromDate = yyyymmdd.parse(from)
+    val toDate = yyyymmdd.parse(to)
+    val cfg = FullRebuild(key, name, fromDate, toDate)
+    scraper.scrape(cfg)
+  }
+
+
+  def update(key: String, name: String, from: String, to: String) {
+    val fromDate = yyyymmdd.parse(from)
+    val toDate = yyyymmdd.parse(to)
+    val cfg = FullRebuild(key, name, fromDate, toDate)
+    scraper.scrape(cfg)
+  }
+
+  def delete(key: String) {
+
+  }
 }
+
--- deepfij-web/src/main/scala/com/fijimf/deepfij/view/AdminPanel.scala
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/view/AdminPanel.scala
@@ -2,10 +2,13 @@ package com.fijimf.deepfij.view
 
 import com.fijimf.deepfij.modelx.ScheduleDao
 import java.text.SimpleDateFormat
+import java.util.Date
 
 object AdminPanel {
   val sd = new ScheduleDao()
   val dateFmt = new SimpleDateFormat("MMM-dd-yyyy")
+  val yyyymmdd = new SimpleDateFormat("yyyyMMdd")
+
   def apply() = {
     <div class="row">
       <div class="span12">
@@ -20,11 +23,13 @@ object AdminPanel {
           <table class="table table-bordered table-condensed">
             <thead>
               <tr>
-                <th>Key</th> <th>Name</th> <th>Conferences</th> <th>Teams</th> <th>Games</th> <th>FIrst</th> <th>Last</th>
+                <th>Key</th> <th>Name</th> <th>Teams</th> <th>Games</th> <th>First</th> <th>Last</th> <th colspan="3"></th>
               </tr>
             </thead>
             <tbody>
               {sd.findAll().map(s => {
+              val lastDate: Date = s.gameList.maxBy(_.date).date
+              val firstDate: Date = s.gameList.minBy(_.date).date
               <tr>
                 <td>
                   {s.key}
@@ -33,22 +38,25 @@ object AdminPanel {
                   {s.name}
                 </td>
                 <td>
-                  {s.conferenceList.size}
-                </td>
-                <td>
                   {s.teamList.size}
                 </td>
                 <td>
                   {s.gameList.size}
                 </td>
                 <td>
-                  {dateFmt.format(s.gameList.minBy(_.date).date)}
+                  {dateFmt.format(firstDate)}
                 </td>
                 <td>
-                  {dateFmt.format(s.gameList.maxBy(_.date).date)}
+                  {dateFmt.format(lastDate)}
                 </td>
                 <td>
-                  <a href={"/admin/rebuild&key="+s.key+"&name="+s.name}>Rebuild</a>
+                  <a class="btn btn-success" href={"/admin/update?key=" + s.key + "&name=" + s.name}>Update</a>
+                </td>
+                <td>
+                  <a class="btn btn-primary" href={"/admin/rebuild?key=" + s.key + "&name=" + s.name + "&from=" + firstDate + "&to=" + lastDate}>Rebuild</a>
+                </td>
+                <td>
+                  <a class="btn btn-danger" href={"/admin/delete?key=" + s.key + "&name=" + s.name}>Delete</a>
                 </td>
               </tr>
             })}
@@ -60,13 +68,13 @@ object AdminPanel {
         <div class="span6">
           <form class="well" method="GET" action="/admin/new">
             <label>Name</label>
-              <input id=" name" type="text" class="span3" placeholder="Schedule name..."/>
+              <input id="name" name="name" type="text" class="span3" placeholder="Schedule name..."/>
             <label>Key</label>
-              <input id="key" type="text" class="span3" placeholder="schedule-key"/>
-            <label class="checkbox">
-                <input type="checkbox"/>
-              Auto-populate?
-            </label>
+              <input id="key" name="key" type="text" class="span3" placeholder="schedule-key"/>
+            <label>From</label>
+              <input id="from" name="from" type="text" class="span3" placeholder="yyyymmdd"/>
+            <label>Key</label>
+              <input id="to" name="to" type="text" class="span3" placeholder="yyyymmdd"/>
             <button type="submit" class="btn btn-primary">New Schedule</button>
           </form>
         </div>
