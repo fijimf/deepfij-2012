f7f8f4f1807465d08c7738ee9991a61fd5e51a57
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-05-16 11:03:15 -0400

    Moving back to scalatra from spray

---
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/server/filter/Controller.scala
@@ -0,0 +1,35 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: fijimf
+ * Date: 5/16/12
+ * Time: 2:32 AM
+ */
+package com.fijimf.deepfij.server.filter
+
+import org.scalatra.ScalatraFilter
+import com.fijimf.deepfij.modelx.TeamDao
+import com.fijimf.deepfij.view.{MissingResourcePanel, TeamPanel, BasePage}
+
+class Controller extends ScalatraFilter {
+  val td = new TeamDao()
+  val scheduleKey = "2012"
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say
+        <a href="hello-scalate">hello to Scalate</a>
+        .
+      </body>
+    </html>
+  }
+
+  get("/team/:key") {
+    val key: String = params("key")
+    println("Building Team Page " + scheduleKey + "," + key)
+    td.findByKey(scheduleKey, key) match {
+      case Some(t) => BasePage(title = t.name, content = Some(TeamPanel(t)))
+      case None => BasePage(title = "Team Not Found", content = Some(MissingResourcePanel("team", key)))
+    }
+  }
+}
