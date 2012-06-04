0c991e5778b474baa926267c8726316b0e89b53c
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-06-04 14:14:47 -0400

    Implementing DB

---
+++ deepfij-model/src/main/scala/com/fijimf/deepfij/statx/StatisticMap.scala
@@ -0,0 +1,15 @@
+package com.fijimf.deepfij.statx
+
+import java.util.Date
+
+case class StatisticMap[T](name: String, higherIsBetter: Boolean, data: Map[(Date, T), Double]) extends Statistic[T] {
+  val (d, k) = data.keys.unzip
+
+  def keys = k.toSet.toList
+
+  def startDate = d.min
+
+  def endDate = d.max
+
+  def function(k: T, d: Date) = data.get((d, k))
+}
