2ff41870284f366cbd72a8482d94e443c1694db8
fijimf <fijimf@gmail.com>
2012-06-05 00:21:49 -0400

    Either permissions or line endings are messed up

--- deepfij-model/src/main/scala/com/fijimf/deepfij/statx/ModelTester.scala
+++ deepfij-model/src/main/scala/com/fijimf/deepfij/statx/ModelTester.scala
@@ -3,7 +3,7 @@ package com.fijimf.deepfij.statx
 import com.fijimf.deepfij.modelx.{Team, Schedule, ScheduleDao}
 import java.util.Date
 import java.text.SimpleDateFormat
-import models.WonLostModel
+import models.PointsModel
 
 
 object ModelTester {
@@ -14,14 +14,14 @@ object ModelTester {
     val sched: Schedule = sd.findByKey("ncaa2012").get
     println(sched.teamList.size)
     println(sched.gameList.size)
-    val model: WonLostModel = new WonLostModel
+    val model: PointsModel = new PointsModel
     println("Starting" + new Date)
     val statistics: Map[String, Statistic[Team]] = model.createStatistics(sched)
     println("Ending " + new Date)
     println(statistics.keys.mkString(","))
-    val atop20: List[(Team, Double)] = statistics("wp").population(new SimpleDateFormat("yyyyMMdd").parse("20120121")).topN(20)
-    val btop20: List[(Team, Double)] = statistics("wp").population(new SimpleDateFormat("yyyyMMdd").parse("20120221")).topN(20)
-    val ctop20: List[(Team, Double)] = statistics("wp").population(new SimpleDateFormat("yyyyMMdd").parse("20120321")).topN(20)
+    val atop20: List[(Team, Double)] = statistics("log-score-ratio-mean").population(new SimpleDateFormat("yyyyMMdd").parse("20120121")).topN(20)
+    val btop20: List[(Team, Double)] = statistics("log-score-ratio-mean").population(new SimpleDateFormat("yyyyMMdd").parse("20120221")).topN(20)
+    val ctop20: List[(Team, Double)] = statistics("log-score-ratio-mean").population(new SimpleDateFormat("yyyyMMdd").parse("20120321")).topN(20)
     println(atop20.map{case (team: Team, d: Double) => "%-32s %5.2f".format(team.name, d)}.mkString("\n"))
     println(btop20.map{case (team: Team, d: Double) => "%-32s %5.2f".format(team.name, d)}.mkString("\n"))
     println(ctop20.map{case (team: Team, d: Double) => "%-32s %5.2f".format(team.name, d)}.mkString("\n"))
