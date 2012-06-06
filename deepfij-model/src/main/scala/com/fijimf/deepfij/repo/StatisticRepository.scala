54c57d93cade54b9bf444fb91777d0b8004275f9
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-06-06 11:25:18 -0400

    Cleaned up statistics model

--- deepfij-model/src/main/scala/com/fijimf/deepfij/repo/StatisticRepository.scala
+++ deepfij-model/src/main/scala/com/fijimf/deepfij/repo/StatisticRepository.scala
@@ -3,9 +3,14 @@ package com.fijimf.deepfij.repo
 import com.fijimf.deepfij.statx.Statistic
 import com.fijimf.deepfij.util.DateStream
 import com.fijimf.deepfij.modelx._
+import org.apache.log4j.Logger
 
 
 class StatisticRepository extends Transactional {
+  val logger = Logger.getLogger(this.getClass)
+
+  val entityManager = PersistenceSource.entityManager
+
   val msd = new MetaStatDao
   val tsd = new TeamStatDao
 
@@ -19,6 +24,7 @@ class StatisticRepository extends Transactional {
 
   def publish(statistic: Statistic[Team]) {
     transactional {
+      logger.info("Publishing " + statistic.statKey)
       msd.findByStatKey(statistic.statKey).foreach(m => msd.delete(m.id))
       val ms = msd.save(new MetaStat(name = statistic.name, statKey = statistic.statKey, format = statistic.format, higherIsBetter = statistic.higherIsBetter))
       for (d <- DateStream(statistic.startDate, statistic.endDate);
