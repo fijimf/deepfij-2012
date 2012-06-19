cacb54d2f1da5bcd4abf81db5a2708a0d70fab0b
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-06-19 15:46:55 -0400

    Schema change

--- deepfij-model/src/main/resources/schema.sql
+++ deepfij-model/src/main/resources/schema.sql
@@ -29,8 +29,8 @@ drop table if exists user;
 drop table if exists user_role;
 create table alias (id bigint not null auto_increment, alias varchar(255) not null, updatedAt datetime, scheduleId bigint, teamId bigint, primary key (id), unique (scheduleId, alias));
 create table conference (id bigint not null auto_increment, keyName varchar(255), name varchar(255), updatedAt datetime, schedule_id bigint not null, primary key (id), unique (schedule_id, keyName), unique (schedule_id, name));
-create table game (id bigint not null auto_increment, date datetime not null, isConferenceTournament bit not null, isNcaaTournament bit not null, isNeutralSite bit not null, updatedAt datetime, awayTeamId bigint, homeTeamId bigint, schedule_id bigint, primary key (id));
-create table metaStat (id bigint not null auto_increment, higherIsBetter bit, keyName varchar(255) unique, name varchar(255) unique, primary key (id));
+create table game (id bigint not null auto_increment, date date not null, isConferenceTournament bit not null, isNcaaTournament bit not null, isNeutralSite bit not null, updatedAt datetime, awayTeamId bigint, homeTeamId bigint, schedule_id bigint, primary key (id));
+create table metaStat (id bigint not null auto_increment, format varchar(255), higherIsBetter bit, name varchar(255) unique, keyName varchar(255) unique, primary key (id));
 create table permission (id bigint not null auto_increment, permission varchar(255) not null unique, updatedAt datetime not null, primary key (id));
 create table quote (id bigint not null auto_increment, quote varchar(255) not null, source varchar(255), updatedAt datetime not null, url varchar(255), primary key (id));
 create table result (id bigint not null auto_increment, awayScore integer, homeScore integer, updatedAt datetime, game_id bigint, primary key (id));
@@ -38,7 +38,7 @@ create table role (id bigint not null auto_increment, name varchar(255) not null
 create table role_permission (permission_id bigint not null, role_id bigint not null, primary key (role_id, permission_id));
 create table schedule (id bigint not null auto_increment, isPrimary bit, keyName varchar(255) unique, name varchar(255) unique, updatedAt datetime, primary key (id));
 create table team (id bigint not null auto_increment, keyName varchar(255) not null, logo varchar(255), longName varchar(255) not null, name varchar(255) not null, nickname varchar(255), officialUrl varchar(255), primaryColor varchar(255), secondaryColor varchar(255), updatedAt datetime, conference_id bigint, schedule_id bigint, primary key (id), unique (schedule_id, keyName), unique (schedule_id, longName), unique (schedule_id, name));
-create table teamStat (id bigint not null auto_increment, date datetime, value double precision, metaStat_id bigint not null, team_id bigint not null, primary key (id), unique (metaStat_id, team_id, date));
+create table teamStat (id bigint not null auto_increment, date date, value double precision, metaStat_id bigint not null, team_id bigint not null, primary key (id), unique (metaStat_id, team_id, date));
 create table user (id bigint not null auto_increment, email varchar(255) not null, password varchar(255) not null, updatedAt datetime not null, primary key (id));
 create table user_role (role_id bigint not null, user_id bigint not null, primary key (user_id, role_id));
 alter table alias add index FK5899650C8E8797B (teamId), add constraint FK5899650C8E8797B foreign key (teamId) references team (id);
@@ -56,9 +56,9 @@ alter table teamStat add index FK9C8B4651A76F8380 (team_id), add constraint FK9C
 alter table teamStat add index FK9C8B465122D71320 (metaStat_id), add constraint FK9C8B465122D71320 foreign key (metaStat_id) references metaStat (id);
 alter table user_role add index FK143BF46A4F4A20E0 (role_id), add constraint FK143BF46A4F4A20E0 foreign key (role_id) references role (id);
 alter table user_role add index FK143BF46AF474E4C0 (user_id), add constraint FK143BF46AF474E4C0 foreign key (user_id) references user (id);
--------BELOW THIS LINE WILL NOT BE AUTO GENERATED-------;
+-------BELOW THIS LINE WILL NOT BE AUTO GENERATED-------
 
-insert into user(email, password, updatedAt) values ('fijimf@gmail.com', '7556a0d1970e9e8b7f5fa40110a6f4f998a66806d079095acc10108b6d610724', '1900-01-01');;
+insert into user(email, password, updatedAt) values ('fijimf@gmail.com', '7556a0d1970e9e8b7f5fa40110a6f4f998a66806d079095acc10108b6d610724', '1900-01-01');
 
 insert into role(name, updatedAt) values ('ADMIN','1900-01-01' );
 insert into role(name, updatedAt) values ('TRUSTED_USER','1900-01-01' );
