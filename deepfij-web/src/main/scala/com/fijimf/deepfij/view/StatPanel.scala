873f3464d2edbb0675ab240fdaae94fdd1fb879d
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-06-19 17:18:48 -0400

    Schema change

--- deepfij-web/src/main/scala/com/fijimf/deepfij/view/StatPanel.scala
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/view/StatPanel.scala
@@ -7,9 +7,10 @@ object StatPanel {
   def apply(key: String): NodeSeq = {
     <div>
       <div id="stat">
-      <h3 id="name"/>
-      <h3 id="mean"/>
-      <h3 id="stddev"/>
+          <h3 id="name"/>
+          <h3 id="mean"/>
+          <h3 id="stddev"/>
+          <div id="shit"/>
       </div>
     </div>
   }
--- deepfij-web/src/main/webapp/scripts/statLoader.js
+++ deepfij-web/src/main/webapp/scripts/statLoader.js
@@ -4,5 +4,20 @@ $(document).ready(function () {
             $('#stat #name').replaceWith(data.name)
             $('#stat #mean').replaceWith(" "+data.mean)
             $('#stat #stddev').replaceWith(" "+data.stdDev)
+
+            var outerW = 500;
+            var outerH = 200;
+            d3.select("#stat #shit").append()
+              var colours = ["#08519C", "#3182BD", "#6BAED6", "#BDD7E7", "#EFF3FF"]; // ColorBrewer Blues
+              var vis;
+              var x0 = function (d) { return d.y0 * w; }; // lower bound
+              var x1 = function (d) { return (d.y0 + d.y) * w; } // upper bound
+              var y = function (d) { return d.x * h / 3; };
+
+                vis = d3.select("#vis")
+                    .append("svg:svg")
+                    .attr("width", outerW)
+                    .attr("height", outerH);
+
         });
 })
