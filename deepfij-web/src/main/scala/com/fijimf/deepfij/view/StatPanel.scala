93ca4a41f771167109b319d4daa41c04e405e7fd
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-06-20 15:25:04 -0400

    fixed ;

--- deepfij-web/src/main/scala/com/fijimf/deepfij/view/StatPanel.scala
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/view/StatPanel.scala
@@ -31,7 +31,7 @@ object StatPanel {
       </div>
       <script>
         {"""$(document).ready(function () {
-      teamStatBarChart("%s")
+      teamStatBarChart('%s')
       })""".format(key)}
       </script>
   }
