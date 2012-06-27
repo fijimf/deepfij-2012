f46b65d41997f2c2c961cbe5f5c9eb7dd00869d8
U-frohnhofer\fijimf <fijimf@frohnhofer.(none)>
2012-06-27 00:53:14 -0400

    Huh?

--- deepfij-web/src/main/scala/com/fijimf/deepfij/server/controller/api/StataPI.scala
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/server/controller/api/StataPI.scala
@@ -14,7 +14,11 @@ trait StatApi {
 
   case class Stat(name: String, yyyymmddd: String, max:Double, min:Double, mean: Double, stdDev: Double, observations: List[Obs])
 
-  case class Obs(name: String, rank: Double, value: Double)
+  case class Obs(name: String, conference:String, color:String, rank: Double, value: Double)
+
+  case class ConfNode(name:String, children:List[Obs])
+
+  case class Root(name:String, children:List[ConfNode])
 
   get("/stat/:key") {
     contentType = "text/html"
@@ -24,6 +28,11 @@ trait StatApi {
     contentType = "application/json"
     val s = std.statistic(params("key"))
     generateJSON(s.name, s.endDate, s.population(s.endDate))
+  }
+  get("/api/stat/bubble/:key") {
+    contentType = "application/json"
+    val s = std.statistic(params("key"))
+    generateByConfJSON(s.name, s.endDate, s.population(s.endDate))
   }
 
   get("/api/stat/:key/:date") {
@@ -37,10 +46,20 @@ trait StatApi {
   def generateJSON(name: String, date: Date, population: Population[Team]): String = {
     val fmt = new SimpleDateFormat("yyyyMMdd")
     val os: List[Obs] = population.topN(population.keys.size).map {
-      case (team: Team, d: Double) => Obs(team.name, population.rank(team).getOrElse(0.0), d)
+      case (team: Team, d: Double) => Obs(team.name,team.conference.name, team.primaryColorOpt.getOrElse(null), population.rank(team).getOrElse(0.0), d)
     }
     val data: Stat = Stat(name, fmt.format(date), population.max.getOrElse(0),population.min.getOrElse(0),population.mean.getOrElse(0), population.stdDev.getOrElse(0), os)
     Json.generate(data)
   }
+
+  def generateByConfJSON(name: String, date: Date, population: Population[Team]): String = {
+    val fmt = new SimpleDateFormat("yyyyMMdd")
+    val os: List[Obs] = population.topN(population.keys.size).map {
+      case (team: Team, d: Double) => Obs(team.name, team.conference.name, team.primaryColorOpt.getOrElse(null), population.rank(team).getOrElse(0.0), d)
+    }
+    val data = Root(name, os.groupBy(_.conference).map{case (confName: String, oss: List[Obs]) => ConfNode(confName, oss)}.toList)
+    Json.generate(data)
+  }
+
 
 }
