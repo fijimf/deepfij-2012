e05f082f848c26d526221d9923eb9c3f16342fb3
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-07-19 12:37:42 -0400

    Starting to put scalatra tests back in

---
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/view/team/TeamPageMap.scala
@@ -0,0 +1,35 @@
+package com.fijimf.deepfij.view.team
+
+import com.fijimf.deepfij.modelx.Team
+import org.apache.shiro.SecurityUtils
+import org.apache.shiro.subject.Subject
+
+
+class TeamPageMap {
+  def apply(team: Team): Map[String, Any] = {
+    val ctx = Map(
+      "name" -> team.name,
+      "key" -> team.key,
+      "conference" -> Map("name" -> team.conference.name, "key" -> team.conference.key),
+      "wins" -> team.wins.size,
+      "losses" -> team.losses.size,
+      "conferenceWins" -> team.wins.filter(g => g.homeTeam.conference == g.awayTeam.conference).size,
+      "conferenceLosses" -> team.losses.filter(g => g.homeTeam.conference == g.awayTeam.conference).size
+    "games" -> team.games.map(g => {
+      val opp = if (g.homeTeam == team) g.awayTeam else g.homeTeam
+      Map(
+        "opponent" -> Map("name" -> opp.name, "key" -> opp.key)
+      ) ++ if
+
+    })
+    )
+    val subject: Subject = SecurityUtils.getSubject
+    val userCtx = if (subject.isRemembered || subject.isAuthenticated) {
+      Map("user" -> subject.getPrincipal)
+    } else {
+      Map.empty[String, Any]
+    }
+    ctx ++ userCtx
+  }
+
+}
