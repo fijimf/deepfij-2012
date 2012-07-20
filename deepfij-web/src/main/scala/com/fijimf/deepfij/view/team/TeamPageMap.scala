0234bcca581fee6dae195ef47ab460870e7df979
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-07-20 16:37:23 -0400

    Switching to mustache for templating

--- deepfij-web/src/main/scala/com/fijimf/deepfij/view/team/TeamPageMap.scala
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/view/team/TeamPageMap.scala
@@ -3,9 +3,12 @@ package com.fijimf.deepfij.view.team
 import com.fijimf.deepfij.modelx.Team
 import org.apache.shiro.SecurityUtils
 import org.apache.shiro.subject.Subject
+import java.text.SimpleDateFormat
 
 
-class TeamPageMap {
+object TeamPageMap {
+  val fmt = new SimpleDateFormat("yyyyMMdd")
+
   def apply(team: Team): Map[String, Any] = {
     val ctx = Map(
       "name" -> team.name,
@@ -14,24 +17,26 @@ class TeamPageMap {
       "wins" -> team.wins.size,
       "losses" -> team.losses.size,
       "conferenceWins" -> team.wins.filter(g => g.homeTeam.conference == g.awayTeam.conference).size,
-      "conferenceLosses" -> team.losses.filter(g => g.homeTeam.conference == g.awayTeam.conference).size
-    "games" -> team.games.map(g => {
-      val opp = if (g.homeTeam == team) g.awayTeam else g.homeTeam
-      val result = if (g.resultOpt.isDefined) {
+      "conferenceLosses" -> team.losses.filter(g => g.homeTeam.conference == g.awayTeam.conference).size,
+      "games" -> team.games.map(g => {
+        val opp = if (g.homeTeam == team) g.awayTeam else g.homeTeam
+
         Map(
-          "wl" -> if (g.isWin(team)) "W" else "L",
-        "score" -> "%d - %d".format(g.resultOpt.get.homeScore, g.resultOpt.get.awayScore)
+          "opponent" -> Map("name" -> opp.name, "key" -> opp.key),
+          "date" -> fmt.format(g.date),
+          "wl" -> (
+            if (g.isWin(team))
+              "W"
+            else if (g.isLoss(team))
+              "L"
+            else
+              ""
+            ),
+          "score" -> g.resultOpt.map(rslt => "%d - %d".format(rslt.homeScore, rslt.awayScore)).getOrElse("")
         )
-      }
-      Map(
-        "opponent" -> Map("name" -> opp.name, "key" -> opp.key)
-      "date" -> g.date.toString
-
-      )
-
-
-    })
+      })
     )
+
     val subject: Subject = SecurityUtils.getSubject
     val userCtx = if (subject.isRemembered || subject.isAuthenticated) {
       Map("user" -> subject.getPrincipal)
