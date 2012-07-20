d88aab0c1d82f354ac51a2c872163aa9d275d9be
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-07-20 16:50:29 -0400

    Switching to mustache for templating

--- deepfij-web/src/main/scala/com/fijimf/deepfij/server/controller/TeamController.scala
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/server/controller/TeamController.scala
@@ -1,14 +1,15 @@
 package com.fijimf.deepfij.server.controller
 
 import com.fijimf.deepfij.view.{MissingResourcePanel, BasePage}
-import com.fijimf.deepfij.view.team.{TeamEditPanel, TeamPanel}
+import com.fijimf.deepfij.view.team.{TeamEditPanel, TeamPageMap}
 
 trait TeamController {
   this: Controller =>
   get("/team/:key") {
     contentType = "text/html"
     td.findByKey(params("key")) match {
-      case Some(t) => BasePage(title = t.name, content = Some(TeamPanel(t))).toHtml5()
+      //case Some(t) => BasePage(title = t.name, content = Some(TeamPanel(t))).toHtml5()
+      case Some(t) => templateEngine.layout("pages/team.mustache", TeamPageMap(t))
       case None => BasePage(title = "Team Not Found", content = Some(MissingResourcePanel("team", params("key")))).toHtml5()
     }
   }
--- deepfij-web/src/main/scala/com/fijimf/deepfij/view/team/TeamPageMap.scala
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/view/team/TeamPageMap.scala
@@ -1,6 +1,6 @@
 package com.fijimf.deepfij.view.team
 
-import com.fijimf.deepfij.modelx.Team
+import com.fijimf.deepfij.modelx.{Game, Team}
 import org.apache.shiro.SecurityUtils
 import org.apache.shiro.subject.Subject
 import java.text.SimpleDateFormat
@@ -19,21 +19,7 @@ object TeamPageMap {
       "conferenceWins" -> team.wins.filter(g => g.homeTeam.conference == g.awayTeam.conference).size,
       "conferenceLosses" -> team.losses.filter(g => g.homeTeam.conference == g.awayTeam.conference).size,
       "games" -> team.games.map(g => {
-        val opp = if (g.homeTeam == team) g.awayTeam else g.homeTeam
-
-        Map(
-          "opponent" -> Map("name" -> opp.name, "key" -> opp.key),
-          "date" -> fmt.format(g.date),
-          "wl" -> (
-            if (g.isWin(team))
-              "W"
-            else if (g.isLoss(team))
-              "L"
-            else
-              ""
-            ),
-          "score" -> g.resultOpt.map(rslt => "%d - %d".format(rslt.homeScore, rslt.awayScore)).getOrElse("")
-        )
+        gameToMap(g, team)
       })
     )
 
@@ -46,4 +32,21 @@ object TeamPageMap {
     ctx ++ userCtx
   }
 
+
+  def gameToMap(g: Game, team: Team): Map[String, Object] = {
+    val opp = if (g.homeTeam == team) g.awayTeam else g.homeTeam
+    Map(
+      "opponent" -> Map("name" -> opp.name, "key" -> opp.key),
+      "date" -> fmt.format(g.date),
+      "wl" -> (
+        if (g.isWin(team))
+          "W"
+        else if (g.isLoss(team))
+          "L"
+        else
+          ""
+        ),
+      "score" -> g.resultOpt.map(rslt => "%d - %d".format(rslt.homeScore, rslt.awayScore)).getOrElse("")
+    )
+  }
 }
