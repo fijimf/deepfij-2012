9f6dde1ca36b619a81383a4221002497d93a799e
fijimf <fijimf@gmail.com>
2012-07-24 00:11:03 -0400

    Quote editing work

--- deepfij-web/src/main/scala/com/fijimf/deepfij/server/controller/QuoteController.scala
+++ deepfij-web/src/main/scala/com/fijimf/deepfij/server/controller/QuoteController.scala
@@ -1,7 +1,6 @@
 package com.fijimf.deepfij.server.controller
 
-import com.fijimf.deepfij.view.{MissingResourcePanel, BasePage}
-import com.fijimf.deepfij.view.schedule.{ScheduleEditPanel, ScheduleShowPanel, ScheduleCreatePanel}
+import com.fijimf.deepfij.modelx.Quote
 
 trait QuoteController {
   this: Controller =>
@@ -12,38 +11,16 @@ trait QuoteController {
     </p>
   }
 
-
-  get("/quote/new") {
-    contentType = "text/html"
-    BasePage(title = "Deep Fij Admin", content = Some(ScheduleCreatePanel())).toHtml5()
-  }
-
   get("/quote/list") {
     contentType = "text/html"
-    templateEngine.layout("pages/quotelist.mustache", Map("quotes" -> qd.findAll().map(q => Map("id" -> q.id, "quotes" -> q.quote, "source" -> q.source, "url" -> q.url))))
+    templateEngine.layout("pages/quotelist.mustache", Map("quotes" -> qd.findAll().map(q => Map("id" -> q.id, "quote" -> q.quote, "source" -> q.source, "url" -> q.url))))
   }
 
   post("/quote/new") {
-    val id = createQuote(params("quote"), params("source"), params("url"))
-    redirect("/quote/show/" + id)
+    createQuote(params("quote"), params("source"), params("url"))
+    redirect("/quote/list")
   }
 
-  get("/quote/show/:id") {
-    contentType = "text/html"
-    sd.findByKey(params("key")) match {
-      case Some(schedule) => BasePage(title = "Deep Fij Admin", content = Some(ScheduleShowPanel(schedule))).toHtml5()
-      case None => BasePage(title = "Deep Fij Admin", content = Some(MissingResourcePanel("schedule", params("key")))).toHtml5()
-    }
-  }
-
-  get("/quote/edit/:id") {
-    contentType = "text/html"
-    val key: String = params("id")
-    sd.findByKey(key) match {
-      case Some(schedule) => BasePage(title = "Deep Fij Admin", content = Some(ScheduleEditPanel(schedule))).toHtml5()
-      case None => BasePage(title = "Deep Fij Admin", content = Some(MissingResourcePanel("schedule", key))).toHtml5()
-    }
-  }
   post("/quote/edit") {
     contentType = "text/html"
     val id: String = params("id")
@@ -51,16 +28,16 @@ trait QuoteController {
   }
 
   post("/quote/delete") {
-    deleteQuote(params("id"))
+    deleteQuote(params("id").toLong)
     redirect("/admin#collapseQuotes")
   }
 
-  def deleteQuote(k: String) {
+  def deleteQuote(id: Long) {
 
   }
 
-  def createQuote(q: String, s: String, u: String): Integer = {
-    0
+  def createQuote(q: String, s: String, u: String) = {
+    qd.save(new Quote(quote = q, source = s, url = u))
   }
 
 }
