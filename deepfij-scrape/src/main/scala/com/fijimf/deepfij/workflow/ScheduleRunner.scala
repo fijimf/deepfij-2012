5d6b77d23b928284b17a9301ccb06cb8f0105591
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2012-09-10 16:40:32 -0400

    Documentation

--- deepfij-scrape/src/main/scala/com/fijimf/deepfij/workflow/ScheduleRunner.scala
+++ deepfij-scrape/src/main/scala/com/fijimf/deepfij/workflow/ScheduleRunner.scala
@@ -6,19 +6,29 @@ import org.apache.log4j.Logger
 import com.fijimf.deepfij.util.Validation._
 
 /**
- * So heres the deal abstracting out to the general case.
+ * Solves the following problem.
  *
- * We want to instantiate some aggregate type composed of other related components on startup
- * and (potentially) keep itself current.  The resources we have are
+ * In the general case:
+ *
+ * We want to instantiate some aggregate type, composed of other related component types on startup
+ * and (potentially) keep the aggregate type current.  The resources we have are
  * (1) Persistence we own, viz. a database;
  * (2) One or more primary sources which we don't own;
  * (3) Knowledge that the components of our Schedule aggregate can be identified by a unique, domain specific key;
  * (4) Ability to construct component objects given the primary source and (possibly partially constructed) aggregate;
  *
+ * To be explicit:
+ * Schedule              <= aggregate
+ * T <: KeyedObject, ... <= types
+ * DataSource[T]         <= source
+ * BaseDao[T,_]          <= persistence
  *
  * Given those parameters we have three main use cases
  *
  * (1) Cold startup <- Rebuild everything; drop any existing data in the database
+ * A. Drop/Create aggregate type from the database
+ * B. In order, over the list of component types
+ * 1. Persist them to the database
  * (2) Warm startup <- Static data is good; reload dynamic data
  * (3) Hot startup  <- Quick restart all data in the database is good and we can just start updater tasks
  */
