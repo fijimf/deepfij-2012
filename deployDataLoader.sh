e9f7b9b8cf33a7b2e6105814a2f9cb3a8ab25344
fijimf <fijimf@gmail.com>
2012-11-17 17:50:31 -0500

    Prepare to release

---
+++ deployDataLoader.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd deepfij-scrape
+
+mvn clean install dependency:copy-dependencies
+
+cd target
+
+mv deepfij-scrape-*.jar deepfij-scrape.jar
+
+ftp -inv fijimf.com <<!
+user ${FTP_USER} ${FTP_PASS}
+binary
+cd scraper/lib
+mdel *.jar
+put deepfij-scrape.jar
+mput dependency/*.jar
+quit
+!
