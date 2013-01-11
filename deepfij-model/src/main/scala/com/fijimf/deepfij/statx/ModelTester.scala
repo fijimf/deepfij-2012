d21bfb4917c3f0b583cd00d1d1d3e0438114b3ad
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2013-01-10 23:55:33 -0500

    LSMR working

--- deepfij-model/src/main/scala/com/fijimf/deepfij/statx/ModelTester.scala
+++ deepfij-model/src/main/scala/com/fijimf/deepfij/statx/ModelTester.scala
@@ -15,15 +15,18 @@ object ModelTester {
     println(sched.teamList.size)
     println(sched.gameList.size)
     //val model = new PointsModel
-    //    val model = new WonLostModel
-    val model = new LeastSquaresModel
+    val model = new WonLostModel
+    // val model = new LeastSquaresModel
     println("Starting" + new Date)
     val statistics: Map[String, Statistic[Team]] = model.createStatistics(sched)
     println("Ending " + new Date)
     println(statistics.keys.mkString(","))
-    val atop20: List[(Team, Double)] = statistics("x").population(new SimpleDateFormat("yyyyMMdd").parse("20120121")).topN(20)
-    val btop20: List[(Team, Double)] = statistics("x").population(new SimpleDateFormat("yyyyMMdd").parse("20120221")).topN(20)
-    val ctop20: List[(Team, Double)] = statistics("x").population(new SimpleDateFormat("yyyyMMdd").parse("20120321")).topN(20)
+    val atop20: List[(Team, Double)] = statistics("wp").population(new SimpleDateFormat("yyyyMMdd").parse("20120121")).topN(20)
+    val btop20: List[(Team, Double)] = statistics("wp").population(new SimpleDateFormat("yyyyMMdd").parse("20120221")).topN(20)
+    val ctop20: List[(Team, Double)] = statistics("wp").population(new SimpleDateFormat("yyyyMMdd").parse("20120321")).topN(20)
+    //    val atop20: List[(Team, Double)] = statistics("x").population(new SimpleDateFormat("yyyyMMdd").parse("20120121")).topN(20)
+    //    val btop20: List[(Team, Double)] = statistics("x").population(new SimpleDateFormat("yyyyMMdd").parse("20120221")).topN(20)
+    //    val ctop20: List[(Team, Double)] = statistics("x").population(new SimpleDateFormat("yyyyMMdd").parse("20120321")).topN(20)
     //    val atop20: List[(Team, Double)] = statistics("score-total-mean").population(new SimpleDateFormat("yyyyMMdd").parse("20120121")).topN(20)
     //    val btop20: List[(Team, Double)] = statistics("score-total-mean").population(new SimpleDateFormat("yyyyMMdd").parse("20120221")).topN(20)
     //    val ctop20: List[(Team, Double)] = statistics("score-total-mean").population(new SimpleDateFormat("yyyyMMdd").parse("20120321")).topN(20)
--- deepfij-model/src/main/scala/com/fijimf/deepfij/statx/models/LeastSquaresModel.scala
+++ deepfij-model/src/main/scala/com/fijimf/deepfij/statx/models/LeastSquaresModel.scala
@@ -6,8 +6,10 @@ import org.apache.mahout.math.{RandomAccessSparseVector, DenseVector, SparseMatr
 import java.util.Date
 import org.apache.mahout.math.solver.LSMR
 import com.fijimf.deepfij.util.DateStream
+import org.apache.log4j.Logger
 
 class LeastSquaresModel extends StatisticalModel[Team] with TeamModel {
+  val log = Logger.getLogger(this.getClass)
   val statX: MetaStat = new MetaStat(statKey = "x", name = "x", format = "%9.5f", higherIsBetter = true)
 
   def statistics = List(statX)
@@ -17,32 +19,25 @@ class LeastSquaresModel extends StatisticalModel[Team] with TeamModel {
   }
 
   def processDate(s: Schedule, date: Date, ctx: ModelContext[Team]): ModelContext[Team] = {
-
+    log.info("Processing " + date)
     val games: List[Game] = s.gameList.filter(g => g.resultOpt.isDefined && !g.date.after(date))
     val teamMap: Map[String, Int] = (games.map(_.homeTeam.key) ++ games.map(_.awayTeam.key)).toSet.toList.sorted.zipWithIndex.toMap
-    if (games.size > teamMap.size) {
-      games.foreach(g => println(g))
-      teamMap.foreach(t => println(t))
-      val a: java.util.Map[java.lang.Integer, RandomAccessSparseVector] = new java.util.HashMap[java.lang.Integer, RandomAccessSparseVector]()
-      games.zipWithIndex.foreach(pair => {
-        val r = new RandomAccessSparseVector(teamMap.size)
-        r.set(teamMap(pair._1.homeTeam.key), 1.0)
-        r.set(teamMap(pair._1.awayTeam.key), -1.0)
-        a.put(pair._2, r)
-      })
-      val A = new SparseMatrix(games.size, teamMap.size, a)
-      0.to(A.numRows() - 1).foreach(i => println(i + " " + A.viewRow(i).toString))
-      val b = new DenseVector(games.map(g => (g.result.homeScore - g.result.awayScore).toDouble).toArray)
-      println(b.toString())
-      val lsmr: LSMR = new LSMR()
-      lsmr.setIterationLimit(100)
-      lsmr.setAtolerance(0.00001)
-      lsmr.setBtolerance(0.00001)
-      val x = lsmr.solve(A, b)
-      teamMap.foldLeft(ctx)((ctx, pair) => ctx.update(statX, date, s.teamByKey(pair._1), x.get(pair._2)))
-    } else {
-      ctx
-    }
+    val a: java.util.Map[java.lang.Integer, RandomAccessSparseVector] = new java.util.HashMap[java.lang.Integer, RandomAccessSparseVector]()
+    games.zipWithIndex.foreach(pair => {
+      val r = new RandomAccessSparseVector(teamMap.size)
+      r.set(teamMap(pair._1.homeTeam.key), 1.0)
+      r.set(teamMap(pair._1.awayTeam.key), -1.0)
+      a.put(pair._2, r)
+    })
+    val A = new SparseMatrix(games.size, teamMap.size, a)
+    val b = new DenseVector(games.map(g => (g.result.homeScore - g.result.awayScore).toDouble).toArray)
+    val lsmr: LSMR = new LSMR()
+    lsmr.setIterationLimit(10000)
+    lsmr.setAtolerance(0.00001)
+    lsmr.setBtolerance(0.00001)
+    val x = lsmr.solve(A, b)
+    teamMap.foldLeft(ctx)((ctx, pair) => ctx.update(statX, date, s.teamByKey(pair._1), x.get(pair._2)))
+
 
   }
 }
