b1b96a3f97d8fc709e088f557655f4edade92800
Jim Frohnhofer <jim.frohnhofer@benchmarksolutions.com>
2013-05-23 00:47:15 -0400

    Some cleanup

---
+++ deepfij-scrape/src/main/scala/com/fijimf/deepfij/twitter/TweetSearch.scala
@@ -0,0 +1,17 @@
+package com.fijimf.deepfij.twitter
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jfrohnhofer
+ * Date: 5/23/13
+ * Time: 12:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class TweetSearch {
+
+}
+
+
+object TweetSearch{
+
+}
